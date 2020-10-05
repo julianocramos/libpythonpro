bf3c1b13ee5d04779050c8d3c9301a4bcd0dcdd3
julianocramos <juliano_ nj@hotmail.com>
2020-10-04 21:52:38 -0300

    Integrando testes de email

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,7 +1,7 @@
 class Enviador:
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
-            raise EmailInvalido('Email de remetente inválido:'.format(remetente))
+            raise EmailInvalido('Email de remetente inválido:', format(remetente))
         return remetente
 
 
