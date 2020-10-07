e0f97b01d42450e3b23279bce2d7fa3e0dde4edb
julianocramos <juliano_ nj@hotmail.com>
2020-10-07 17:27:36 -0300

    Incluindo testes e Mock

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -2,15 +2,12 @@ import requests
 
 
 def buscar_avatar(usuario):
-    '''
+    """
     Busca o avatar de um usuário no Github
+
     :param usuario: str busca o nome de um usuário no Github
     :return:str com o link do avatar
-    '''
-    url = 'https://api.github.com/users/{}'.format(usuario)
+    """
+    url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
-
-
-if __name__ == '__main__':
-    print(buscar_avatar('julianocramos'))
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,25 +1,17 @@
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
-    usuario = Usuario(nome='Juliano')
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Juliano', email='juliano_nj@hotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
-    usuarios = [Usuario(nome='Juliano'), Usuario(nome='Renzo')]
+def test_listar_usuarios(sessao):
+    usuarios = [
+        Usuario(nome='Juliano', email='juliano_nj@hotmail.com'),
+        Usuario(nome='Luciano', email='juliano_nj@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
